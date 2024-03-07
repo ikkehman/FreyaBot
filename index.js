e544f05fe4de1a69c02b6971508ff9bf4d04f1ff
Raimuko Yoasu <62538390+ikkehman@users.noreply.github.com>
2024-03-07 17:09:14 +0700

    cronjob pengingat sahur

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth  } = require('whatsapp-web.js');
 const axios = require('axios');
+const schedule = require('node-schedule');
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -21,6 +22,19 @@ client.on('ready', () => {
 //     }
 //     console.log(msg);
 // });
+let number = ['6281270655552'];
+const rule = new schedule.RecurrenceRule();
+rule.hour = 3;
+rule.minute = 0;
+rule.second = 0
+
+const job = schedule.scheduleJob(rule, async function() {
+    for (let i = 0; i < number.length; i++) {
+        await client.sendMessage(`${number[i]}@c.us`, 'Udah jam 3 nih.');
+        await client.sendMessage(`${number[i]}@c.us`, 'Jangan lupa sahur ya! Semangat puasanya! 🌙');
+    }  
+});
+
 client.on('message', async (message) => {
 	// await client.sendMessage(message.from, 'pong');
     try {
@@ -28,7 +42,11 @@ client.on('message', async (message) => {
             messages: message.body
         });
         await client.sendMessage(message.from, response.data.reply);
-        console.log(message.body)
+        const user = await message.getContact();
+        // Mendapatkan nama pengguna
+        const userName = user.name;
+
+        console.log(`Pesan dari: ${userName} - ${message.body}`);
         // console.log('Response from server:', response);
     } catch (error) {
         console.error('Error sending POST request:', error);
